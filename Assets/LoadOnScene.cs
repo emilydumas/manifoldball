5ea92c1fd10da20e12c94a7a9a70a5ca7dd00a44
Horalia <harmas2@uic.edu>
2017-02-24 15:01:23 -0600

    Main Menu: Before scripting

---
+++ Assets/LoadOnScene.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadOnClick : MonoBehaviour {
+
+	public GameObject loadingImage;
+
+	public void LoadScene(int level)
+	{
+		loadingImage.SetActive(true);
+		Application.LoadLevel(level);
+	}
+}
