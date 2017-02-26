0f029805ecc6ef61cc7dc20dd31db92dec223906
David Dumas <david@dumas.io>
2017-02-25 23:21:44 -0600

    Enable main menu scene selection with hand-tracking laser pointer

---
+++ Assets/Scripts/LaserSelect.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class LaserSelect : MonoBehaviour
+{
+    public OVRInput.Controller controller;
+    public LayerMask selectableLayers;
+    private GameObject pointerDest;
+    private bool clicked = false;
+
+    private void activateObject(GameObject ob)
+    {
+        var pe = new PointerEventData(EventSystem.current);
+        ExecuteEvents.Execute(ob, pe, ExecuteEvents.pointerEnterHandler);
+    }
+    private void deactivateObject(GameObject ob)
+    {
+        var pe = new PointerEventData(EventSystem.current);
+        ExecuteEvents.Execute(ob, pe, ExecuteEvents.pointerExitHandler);
+    }
+    private void clickObject(GameObject ob)
+    {
+        var pe = new PointerEventData(EventSystem.current);
+        ExecuteEvents.Execute(ob, pe, ExecuteEvents.submitHandler);
+    }
+
+    private void FixedUpdate()
+    {
+        RaycastHit[] hits;
+        hits = Physics.RaycastAll(transform.position, transform.forward, 100.0f, selectableLayers);
+        if (hits.Length > 0)
+        {
+            int closestHit = 0;
+            for (int i = 0; i < hits.Length; i++)
+            {
+                if (hits[i].distance < hits[closestHit].distance) closestHit = i;
+            }
+    
+            if (hits[closestHit].transform.gameObject != pointerDest)
+            {
+                // Inform objects that a new one is active
+                if (pointerDest != null) deactivateObject(pointerDest);
+                pointerDest = hits[closestHit].transform.gameObject;
+                activateObject(pointerDest);
+            }
+
+            if (!clicked && OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger, controller))
+            {
+                clicked = true;
+                clickObject(pointerDest);
+            }
+        } else {
+            deactivateObject(pointerDest);
+            pointerDest = null;
+            clicked = false;
+        }
+        clicked = OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger, controller);
+    }
+}
---
+++ Assets/Scripts/MenuActions.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuActions : MonoBehaviour {
+    // Functions called by onClick events of main menu buttons.
+    public void LoadScene(string name)
+    {
+        Debug.Log("Switching to scene: " + name);
+        SceneManager.LoadScene(name);
+    }
+}
