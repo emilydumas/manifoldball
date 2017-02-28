1a9c894f51f705eb6322e01e736840867a5a7689
Brandon-Reichman <breich5@uic.edu>
2017-02-27 20:51:22 -0600

    Attempting to fix bounce

--- Assets/Scripts/ManualBouncePhysics.cs
+++ Assets/Scripts/ManualBouncePhysics.cs
@@ -9,22 +9,37 @@ public class ManualBouncePhysics : MonoBehaviour {
     ///   built-in rigid body collision engine.
     /// </summary>
 
-    public float forceMultiplier = 1.0f;
+    public float forceMultiplier = 10000.0f;
     public OVRInput.Controller racketController;
     public GameObject racketGameObject;
 	private Rigidbody rigidBody;
+	Vector3 gravity=new Vector3(0,-0.5f,0);
+	public float threshold = 7.0f;
+
     
 	void Start() {
 		// Store the rigid body so GetComponent does not have to be called 
 		// everytime a collision happens
-		rigidBody = GetComponent<Rigidbody> ();    
+		rigidBody = GetComponent<Rigidbody> ();
+		Physics.gravity = gravity;
 	}
 
     private void OnCollisionEnter(Collision col)
     {
         if (col.gameObject == racketGameObject)
-        {
-            rigidBody.AddForce(OVRInput.GetLocalControllerVelocity(racketController) * forceMultiplier, ForceMode.VelocityChange);
+        {			
+			Vector3 controllerVelocity = OVRInput.GetLocalControllerVelocity (racketController);
+			Vector3 extraVel = controllerVelocity * forceMultiplier;
+
+			// The problem happens when either the ball or the paddle is moving at a very low velocity
+			if (rigidBody.velocity.magnitude < threshold)
+				extraVel = extraVel * 5f;
+
+			//if (controllerVelocity.magnitude < threshold)
+			//	extraVel = ( controllerVelocity + rigidBody.velocity ) * 0.5f; 
+
+
+			rigidBody.AddForce(extraVel, ForceMode.VelocityChange);
         }
     }
 }
