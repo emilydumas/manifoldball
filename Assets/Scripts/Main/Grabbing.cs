c6c019b613ac8c8cf83e24e1e1a376cd943a935a
Brandon-Reichman <breich5@uic.edu>
2017-04-25 19:42:32 -0500

    Very Buggy Grab added
    Wrote a grabbing script from scratch. Allows you to grab ball, cannnot
    throw, its very slow as I probably didn't write the script in the most
    optimal way and if theres any velocity on the ball its more glitchy.
    However you can grab and place the ball around.

---
+++ Assets/Scripts/Main/Grabbing.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grabbing : MonoBehaviour {
+
+	public float grabDistance;
+
+	private Matrix4x4 varBall,varHand;
+	private Vector4 ballPose, handPose;
+	private Material ball;
+	private Material hand;
+	private bool grabbed=false;
+	private float grab;
+	// Use this for initialization
+	void Start () 
+	{
+		ball = GameObject.Find ("Ball").GetComponent<Renderer> ().sharedMaterial;
+		hand = GameObject.Find ("LHand").GetComponent<Renderer> ().sharedMaterial;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		grab = Input.GetAxis ("LGrab");
+		varBall = ball.GetMatrix("_objectpose");
+		varHand = hand.GetMatrix("_objectpose");
+		ballPose = varBall.GetColumn (3);
+		handPose = varHand.GetColumn (3);
+		float distance = Mathf.Sqrt (
+				(ballPose.x - handPose.x) * (ballPose.x - handPose.x) +
+				(ballPose.y - handPose.y) * (ballPose.y - handPose.y) +
+				(ballPose.z - handPose.z) * (ballPose.z - handPose.z));
+		if (distance < grabDistance && grab > 0) {
+			varBall.SetColumn (3, handPose);
+			ball.SetMatrix ("_objectpose", varBall);
+		} else {
+			varBall.SetColumn (3, ballPose);
+			ball.SetMatrix ("_objectpose", varBall);
+		}
+			
+	}
+}
